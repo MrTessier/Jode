06a3a83dffacf2274d065173c9635fe4ebb4c1eb
Michael Tessier <mrtessier@hotmail.com>
2019-04-09 18:39:42 -0400

    Added instructions on how to connect to sql database.

--- Node/Applications/FirstApp/firstApp.js
+++ Node/Applications/FirstApp/firstApp.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var path = require('path');
+const Sequelize = require('sequelize');
 
+console.log("Starting jode application");
 var app = express();
+const sequelize = new Sequelize('LMS', 'mylocalSeanMcintire', 'PASSWORD', {
+    host: '66.59.109.170',
+    dialect: 'mssql'
+});
 
 app.use(express.static("public"))
     .get('/jode', function (req, res) {
@@ -23,6 +29,20 @@ app.use(express.static("public"))
         params.iconClass = req.params.pIconClass;
         params.iconColor = req.params.pColor;
         res.render('ItemCard.ejs', params);
+    })
+    .get('/jode/Test', function (req, res) {
+        let jsonData;
+        console.log('Creating /jode/Test view.');
+
+        sequelize
+            .query("SELECT * FROM AspNetUsers")
+            .then(myTableRows => {
+                console.log('Query Finished');
+                jsonData = JSON.stringify(myTableRows); // Convert result to JSON string.
+                let params = {};
+                params.jsonUsers = jsonData;
+                res.render('Test.ejs', params);
+            })
     })
     .use(function (req, res, next) {
         res.setHeader('Content-Type', 'text/plain');
